eabe46aef29d8bdaa3f3f9392c394d9098005038
Yayoshi <sagauyayoshi@gmail.com>
2019-05-25 19:49:06 +0900

    requests.json()で、文字列型から辞書型に変更して、テキストデータを取得することが できる。県名だけとかを抽出することができる。

--- request_sample01.py
+++ request_sample01.py
@@ -1,5 +1,12 @@
 import requests
+#岩手県八幡平市大更 の情報を requestsモジュールで取得
 
-response = requests.get(url="http://hachimantai.spartacamp.jp/")
+response = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111")
 
 print(response)
+
+# 演習2;別の郵便番号で取得
+response_01 = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=8370926")
+
+data = response.json()
+
