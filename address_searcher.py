59dfb22ef02cbd4c602d34358690325151cf1a3d
Yayoshi <sagauyayoshi@gmail.com>
2019-06-03 21:36:08 +0900

    リファクタリングをした。base_urlを作り、AddressSearcherのインスタンス変数にした。

--- address_searcher.py
+++ address_searcher.py
@@ -2,15 +2,20 @@ import requests
 
 
 class AddressSearcher:
+    def __init__(self):
+        self.base_url = "http://zipcloud.ibsnet.co.jp/api/search"
 
     def search(self, postal_code):
-        url = f"http://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}"
+        url = f"{self.base_url}?zipcode={postal_code}"
 
         response = requests.get(url)
+
         response_dict = response.json()
 
         prefacture = response_dict["results"][0]["address1"]
+
         city = response_dict["results"][0]["address2"]
+
         town = response_dict["results"][0]["address3"]
 
         return f"{prefacture}{city}{town}"
