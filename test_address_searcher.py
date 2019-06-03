24a4402ae2cde8601d3fd3d7bd3d8d3b4e0ecc30
Yayoshi <sagauyayoshi@gmail.com>
2019-06-03 21:02:01 +0900

    APIから地域名を取得できるようになった。 岩手県八幡平市大更はテストをパスしたが、東京都の方はできてない

--- test_address_searcher.py
+++ test_address_searcher.py
@@ -1,11 +1,21 @@
 import unittest
+import requests
 
 
 class AddressSearcher:
 
     def search(self, postal_code):
-        return "岩手県八幡平市大更"
+        url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111"
 
+        response = requests.get(url)
+        response_dict = response.json()
+
+        prefacture = response_dict["results"][0]["address1"]
+        city = response_dict["results"][0]["address2"]
+        town = response_dict["results"][0]["address3"]
+
+        return f"{prefacture}{city}{town}"
+    
 
 class TestAddressSearcher(unittest.TestCase):
     def test_岩手県八幡平市大更の地名を郵便番号から取得できる(self):
@@ -15,7 +25,7 @@ class TestAddressSearcher(unittest.TestCase):
 
         self.assertEqual("岩手県八幡平市大更", actual)
 
-    def test_東京都練馬区豊玉南区郵便番号から取得できる(self):
+    def test_東京都練馬区豊玉南区の地名を郵便番号から取得できる(self):
         address_searcher = AddressSearcher()
 
         actual = address_searcher.search(postal_code="0287111")
